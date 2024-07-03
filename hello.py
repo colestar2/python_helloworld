f9c5927fb1859928ff003ff1d9bd22e4c5bd478d
root <leewhdgur@gmail.com>
2024-07-03 17:27:16 +0900

    edit hello.py

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # 루트 경로 '/' 에 대한 요청을 처리하는 함수를 정의합니다.
 @app.route('/')
 def hello():
-    return '안녕하세요'
+    return '누구세요?'
 
 # 이 스크립트가 메인으로 실행될 때, Flask 애플리케이션을 실행합니다.
 if __name__ == '__main__':
